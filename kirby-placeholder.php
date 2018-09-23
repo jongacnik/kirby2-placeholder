4c417c41851451c333c73642e64dbc813af21403
Jon Gacnik <jon@folderstudio.com>
2018-09-22 17:37:03 -0700

    base64 methods

--- kirby-placeholder.php
+++ kirby-placeholder.php
@@ -3,12 +3,21 @@
 /**
  * KirbyPlaceholder
  *
+ * $file->placeholder()
+ * -> data:image/png;base64, xxxxxxxxxxx
+ *
+ * $site->placeholder(100, 100)
+ * -> data:image/png;base64, xxxxxxxxxxx
+ *
+ * also provides route for dynamic placeholders
  * <img src="/kp?size=400x150" />
  *
  */
 
 namespace KirbyPlaceholder;
 use c;
+use site;
+use file;
 
 class KirbyPlaceholder {
   public static function register () {
@@ -23,18 +32,38 @@ class KirbyPlaceholder {
           $getsize = isset($_GET['size']) ? $_GET['size'] : '1x1';
           $dimensions = explode('x', $getsize);
 
-          // Create image
-          $image = imagecreate($dimensions[0], $dimensions[1]);
-
-          // make transparent
-          $black = imagecolorallocate($image, 0, 0, 0);
-          imagecolortransparent($image, $black);
-
-          // Render image
-          imagepng($image);
+          self::makeImage($dimensions[0], $dimensions[1]);
         }
       ]
     ]);
+
+    site::$methods['placeholder'] = function ($page, $width = 0, $height = 0) { 
+      ob_start (); 
+        self::makeImage($width, $height);
+        $image_data = ob_get_contents (); 
+      ob_end_clean (); 
+      return 'data:image/png;base64, ' . base64_encode($image_data);
+    };
+
+    file::$methods['placeholder'] = function ($file, $reduce = 1) { 
+      ob_start (); 
+        self::makeImage($file->width()/$reduce, $file->height()/$reduce);
+        $image_data = ob_get_contents (); 
+      ob_end_clean (); 
+      return 'data:image/png;base64, ' . base64_encode($image_data);
+    };
+  }
+
+  private static function makeImage ($width, $height) {
+    // Create image
+    $image = imagecreate($width, $height);
+
+    // make transparent
+    $black = imagecolorallocate($image, 0, 0, 0);
+    imagecolortransparent($image, $black);
+
+    // Render image
+    imagepng($image);
   }
 
   public static function url ($width = 0, $height = 0) {
